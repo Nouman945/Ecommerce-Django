0fd523d83348b1f515981ce7040e24c209360dc4
Nouman945 <muhmmadnouman945@gmail.com>
2021-06-01 22:37:25 -0700

    "2nd Part"

--- category/models.py
+++ category/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    catagory_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/catagories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        # verbose_name = "pizza"
+
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.catagory_name
