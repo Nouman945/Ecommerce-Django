ac248d1cee83ba19b1ebfe0227abe4e75b25f9e0
Nouman945 <muhmmadnouman945@gmail.com>
2021-06-30 03:24:18 -0700

    "Starting Cart"

--- ecom/urls.py
+++ ecom/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path('store/',include('store.urls'))
+    path('store/',include('store.urls')),
+    path('cart/',include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
